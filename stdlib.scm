d13d8f47e21556a35a94f68c845a2d57379cf296
mingang.he <dustgle@gmail.com>
2015-07-02 20:48:48 +0800

    define map and apply

--- stdlib.scm
+++ stdlib.scm
@@ -1,2 +1,14 @@
+(define (map proc items)
+  (if (null? items)
+    '()
+    (cons (proc (car items))
+          (map proc (cdr items)))))
+
+(define (apply proc args)
+  (eval
+    (cons proc
+          (map (lambda (x) (list 'quote x))
+               args))))
+
 (define (not x)
   (if x #f #t))
