9760f339badedc7c420c5fe72d524f4761e5b95e
mingang.he <dustgle@gmail.com>
2015-07-18 15:40:51 +0800

    stdlib.scm define reverse

--- stdlib.scm
+++ stdlib.scm
@@ -51,3 +51,9 @@
    (if (null? list1)
        list2
        (cons (car list1) (append (cdr list1) list2))))
+
+(define (reverse items) 
+   (if (null? (cdr items)) 
+       items 
+       (append (reverse (cdr items)) 
+               (cons (car items) '()))))
